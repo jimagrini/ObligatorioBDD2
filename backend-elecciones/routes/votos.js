a4be6b77034a88693b9d50c76a318228a8116377
Andychristoff <andychristoff@gmail.com>
2025-06-25 20:45:55 -0300

    API GET votos totales

--- backend-elecciones/routes/votos.js
+++ backend-elecciones/routes/votos.js
@@ -2,14 +2,18 @@ const express = require('express');
 const router = express.Router();
 const { registrarVoto } = require('../controllers/votosController');
 const { verificarToken, verificarRol } = require('../middlewares/auth');
+const { obtenerVotos } = require('../controllers/votosController');
 
 // Solo funcionarios pueden registrar votos
 router.post(
-'/',
+'/registrarVoto',
 verificarToken,
 verificarRol('FUNCIONARIO'),
 registrarVoto
 );
+
+// GET todos los votos - Solo para funcionarios
+router.get('/', verificarToken, verificarRol('FUNCIONARIO'), obtenerVotos);
 
 module.exports = router;
 
