05d055de7363137de609e6804a9c1fc40cdbd058
Andychristoff <andychristoff@gmail.com>
2025-06-26 16:49:40 -0300

    POST insertarLista API

--- backend-elecciones/routes/listas.js
+++ backend-elecciones/routes/listas.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { verificarToken } = require('../middlewares/auth');
-const { obtenerListas } = require('../controllers/listasController');
+const { verificarToken, verificarRol } = require('../middlewares/auth');
+const { obtenerListas, insertLista } = require('../controllers/listasController');
 
 router.get('/', verificarToken, obtenerListas);
+
+router.post('/insertarLista', verificarToken, verificarRol('FUNCIONARIO') , insertLista);
+
 
 module.exports = router;
